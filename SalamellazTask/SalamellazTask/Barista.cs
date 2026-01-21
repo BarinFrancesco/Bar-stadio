6d2dac354b71f207fd1c8cf897c8f5b7e82246d6
12999355@itisrossi.vi.it <12999355@itisrossi.vi.it>
2026-01-21 17:57:13 +0100

    inizio

---
+++ SalamellazTask/SalamellazTask/Barista.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalamellazTask
+{
+    public  class Barista
+    {
+        Random generatore;
+        public bool Chiuso;
+        public Barista()
+        {
+            generatore = new Random();
+        }
+
+
+        public async void Aspetta()
+        {
+            int tempo = generatore.Next(1, 15);
+            tempo *= 1000;
+
+            await Task.Delay(tempo);
+            
+
+        }
+
+    }
+}
---
+++ SalamellazTask/SalamellazTask/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SalamellazTask
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int nmax = 5;
+
+            Semaphore PostiDisponibili = new Semaphore(nmax, nmax);
+
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
